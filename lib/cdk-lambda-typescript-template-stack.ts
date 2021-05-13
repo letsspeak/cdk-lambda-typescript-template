2093b33e2cc7b365f029bd7d7c443f39d1a36a80
letsspeak <letsinfinite@gmail.com>
2021-05-13 10:20:16 +0900

    first commit

--- lib/cdk-lambda-typescript-template-stack.ts
+++ lib/cdk-lambda-typescript-template-stack.ts
@@ -1,9 +1,20 @@
-import * as cdk from '@aws-cdk/core';
+import events = require('@aws-cdk/aws-events');
+import targets = require('@aws-cdk/aws-events-targets');
+import lambda = require('@aws-cdk/aws-lambda-nodejs');
+import cdk = require('@aws-cdk/core');
+
 
 export class CdkLambdaTypescriptTemplateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create lambda function
+    const testFunc = new lambda.NodejsFunction(this, "test-func");
+
+    // create Event Bridge Trigger to fire testFunc
+    const rule = new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.expression('rate(5 minutes)')
+    });
+    rule.addTarget(new targets.LambdaFunction(testFunc));
   }
 }
